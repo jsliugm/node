011d7047d3017e9791b359acec5da5464d2ad937
liuguangming <jsliugm@gmail.com>
2020-02-04 20:31:03 +0800

    first commit

---
+++ 02回调函数/非阻塞/main.js
@@ -0,0 +1,7 @@
+var fs = require("fs");
+
+fs.readFile('input.txt',function(err,data){
+	
+});
+
+console.log("程序执行结束");
---
+++ workspace/01helloworld/server.js
@@ -0,0 +1,17 @@
+//步骤一、引入http模块
+var http = require('http');
+//步骤二、创建服务器
+http.createServer(
+    function (request, response) {
+        //发送HTTP头部
+        //HTTP状态值：200：ok
+        //内容类型：text/plain
+        response.writeHead(200, {
+            'Content-Type': 'text/plain'
+        });
+
+        //发送响应数据
+        response.end('Hello World\n');
+    }).listen(8888);
+//终端打印如下信息
+console.log('Server running at http://127.0.0.1:8888/');
